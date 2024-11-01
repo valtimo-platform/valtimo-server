89de99b9219bb32534f30ed640560c7d00ec6582
Ivo Zaal <ivo.zaal@ritense.com>
2024-11-01 15:34:10 +0100

    Fixed tests

--- authorization/src/test/kotlin/com/ritense/authorization/deployment/PermissionDeployerIntTest.kt
+++ authorization/src/test/kotlin/com/ritense/authorization/deployment/PermissionDeployerIntTest.kt
@@ -47,7 +47,7 @@ internal class PermissionDeployerIntTest : BaseIntegrationTest() {
         assertThat(changeset.get().filename).endsWith("/testdocument.permission.json")
         assertThat(changeset.get().dateExecuted).isBetween(Instant.parse("2023-06-13T00:00:00Z"), Instant.now())
         assertThat(changeset.get().orderExecuted).isBetween(0, 1000)
-        assertThat(changeset.get().md5sum).isEqualTo("20901dff5a9641278a8ef27f961bbb71")
+        assertThat(changeset.get().md5sum).isEqualTo("7ab83105f788211f135e89686e5f2451")
     }
 
     @Test
