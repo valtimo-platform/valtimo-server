768f36c9f25b62104d6c617f1f902b3f6d2d0d96
floris-thijssen-ritense <floris.thijssen@ritense.com>
2024-11-15 13:12:17 +0100

    story/remove-transactional-from-permission-resource remove transactional from permission resource (#1644)

--- authorization/src/main/kotlin/com/ritense/authorization/web/PermissionResource.kt
+++ authorization/src/main/kotlin/com/ritense/authorization/web/PermissionResource.kt
@@ -26,7 +26,6 @@ import com.ritense.valtimo.contract.domain.ValtimoMediaType.APPLICATION_JSON_UTF
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -42,7 +41,6 @@ class PermissionResource(
     private val logger: Logger = LoggerFactory.getLogger(PermissionResource::class.java)
 
 
-    @Transactional
     @PostMapping("/v1/permissions")
     fun userHasPermission(@RequestBody permissionsPresentRequest: List<PermissionAvailableRequest>)
         : ResponseEntity<List<PermissionAvailableResult>> {
