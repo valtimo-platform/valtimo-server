47475d69b619be294259305896e170782ce464bb
Marijn Verbeek <marijn.verbeek@ritense.com>
2025-01-14 11:30:19 +0100

    added invocation of lambdas for processdocument requests

--- process-document/src/main/java/com/ritense/processdocument/service/impl/CamundaProcessJsonSchemaDocumentService.java
+++ process-document/src/main/java/com/ritense/processdocument/service/impl/CamundaProcessJsonSchemaDocumentService.java
@@ -156,6 +156,8 @@ public class CamundaProcessJsonSchemaDocumentService implements ProcessDocumentS
                 )
             );
 
+            request.doAdditionalModifications(document);
+
             return new NewDocumentAndStartProcessResultSucceeded(
                 document,
                 camundaProcessInstanceId
@@ -209,6 +211,8 @@ public class CamundaProcessJsonSchemaDocumentService implements ProcessDocumentS
 
             final var document = modifyDocumentResult.resultingDocument().orElseThrow();
 
+            request.doAdditionalModifications(document);
+
             AuthorizationContext.runWithoutAuthorization(
                 () -> {
                     camundaTaskService.completeTaskWithFormData(request.taskId(), request.getProcessVars());
@@ -255,6 +259,8 @@ public class CamundaProcessJsonSchemaDocumentService implements ProcessDocumentS
                 processName
             );
 
+            request.doAdditionalModifications(document);
+
             return new NewDocumentForRunningProcessResultSucceeded(
                 document,
                 processInstanceId
@@ -300,6 +306,9 @@ public class CamundaProcessJsonSchemaDocumentService implements ProcessDocumentS
                 UUID.fromString(document.id().toString()),
                 processInstanceWithDefinition.getProcessDefinition().getName()
             ));
+
+            request.doAdditionalModifications(document);
+
             return new ModifyDocumentAndStartProcessResultSucceeded(document, camundaProcessInstanceId);
         } catch (RuntimeException ex) {
             return new ModifyDocumentAndStartProcessResultFailed(parseAndLogException(ex));
@@ -339,6 +348,9 @@ public class CamundaProcessJsonSchemaDocumentService implements ProcessDocumentS
                 document.id().getId(),
                 processInstanceWithDefinition.getProcessDefinition().getName()
             ));
+
+            request.doAdditionalModifications(document);
+
             return new StartProcessForDocumentResultSucceeded(document, camundaProcessInstanceId);
         } catch (RuntimeException ex) {
             return new StartProcessForDocumentResultFailed(parseAndLogException(ex));
