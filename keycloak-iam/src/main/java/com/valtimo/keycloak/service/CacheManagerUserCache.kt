bbc777baeb08f9cf82fefea804099dfc088d931a
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2025-02-03 16:53:58 +0100

    Add UserCache TTL

--- keycloak-iam/src/main/java/com/valtimo/keycloak/service/CacheManagerUserCache.kt
+++ keycloak-iam/src/main/java/com/valtimo/keycloak/service/CacheManagerUserCache.kt
@@ -16,33 +16,42 @@
 
 package com.valtimo.keycloak.service
 
-import com.ritense.valtimo.contract.annotation.AllOpen
+import com.ritense.valtimo.contract.annotation.SkipComponentScan
 import mu.KotlinLogging
 import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 
-@AllOpen
+@Component
+@SkipComponentScan
 class CacheManagerUserCache(
     private val cacheManager: CacheManager,
 ) : UserCache {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T> get(cacheType: CacheType, key: String, requestFunction: (String) -> T?): T? {
-        val cacheName = "${cacheType.name}_${cacheType.cachedClass.simpleName}"
-        val cache = cacheManager.getCache(cacheName)
+        val cache = cacheManager.getCache(cacheType.getCacheName())
             ?: return requestFunction(key)
         val wrapper = cache.get(key)
         if (wrapper != null) {
-            logger.debug("Returning user information from cache {} with key {}.", cacheName, key)
+            logger.debug { "Returning user information from cache ${cacheType.getCacheName()} with key $key." }
             return wrapper.get() as T
         }
 
-        logger.debug("Cache miss for {} with key {}. Adding user to cache.", cacheName, key)
+        logger.debug { "Cache miss for ${cacheType.getCacheName()} with key $key. Adding user to cache." }
         val newValue = requestFunction(key)
         if (newValue != null && !cacheType.cachedClass.java.isAssignableFrom(newValue!!::class.java)) {
             throw IllegalArgumentException("The type of the value returned by the request function (${newValue!!::class.java.name}) does not match the cache type (${cacheType.cachedClass.java.name})")
         }
         cache.put(key, newValue)
         return newValue
+    }
+
+    @CacheEvict(allEntries = true, value = ["EMAIL_ManageableUser", "USER_IDENTIFIER_ValtimoUser"])
+    @Scheduled(fixedRateString = "\${valtimo.keycloak.cache.ttl:PT1H}")
+    fun logCacheClear() {
+        logger.debug { "Clearing all user information cache" }
     }
 
     companion object {
